5ba9c4f9295ecfcab102a3778f307a96ede06a91
of3jlmssoo <7a4wiy5vd638@gmail.com>
2024-02-19 17:24:13 +0900

    router initial

--- surveyapp/lib/main.dart
+++ surveyapp/lib/main.dart
@@ -6,6 +6,7 @@
 // TODO : firestore collection/doc構成決定
 // TODO : firebase/firestore emulator構成
 // TODO : firestoreへ送付
+// TODO : graphic package
 
 // MEMO : DateTimeはエンコードする時にtoIso8601String()でISO8601形式の文字列に変換する
 
@@ -23,13 +24,13 @@ final GoRouter _router = GoRouter(
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
-        return const FirstScreen();
+        return HomeScreen();
       },
       routes: <RouteBase>[
         GoRoute(
           path: 'details',
           builder: (BuildContext context, GoRouterState state) {
-            return const DetailsScreen();
+            return HomeScreen();
           },
         ),
       ],
@@ -52,50 +53,130 @@ class SurveyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SurveyMenu(),
+    return MaterialApp.router(
+      routerConfig: _router,
     );
   }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return const MaterialApp(
+  //     home: SurveyMenu(),
+  //   );
+  // }
 }
 
-class SurveyMenu extends StatefulWidget {
-  const SurveyMenu({super.key});
+// class SurveyMenu extends StatefulWidget {
+//   const SurveyMenu({super.key});
+//
+//   @override
+//   State<SurveyMenu> createState() => _SurveyMenuState();
+// }
+//
+// class _SurveyMenuState extends State<SurveyMenu> with TickerProviderStateMixin {
+//   late AnimationController controller;
+//
+//   final Document document = Document();
+//
+//   var i = 0.0;
+//
+//   @override
+//   void initState() {
+//     controller = AnimationController(
+//       /// [AnimationController]s can be created with `vsync: this` because of
+//       /// [TickerProviderStateMixin].
+//       vsync: this,
+//       duration: const Duration(seconds: 120),
+//     )..addListener(() {
+//         setState(() {});
+//       });
+//     controller.repeat(reverse: true);
+//     super.initState();
+//   }
+//
+//   @override
+//   void dispose() {
+//     controller.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         actions: <Widget>[
+//           MenuAnchor(
+//             builder: (BuildContext context, MenuController controller,
+//                 Widget? child) {
+//               return IconButton(
+//                 onPressed: () {
+//                   if (controller.isOpen) {
+//                     controller.close();
+//                   } else {
+//                     controller.open();
+//                   }
+//                 },
+//                 icon: const Icon(Icons.density_medium),
+//                 tooltip: 'Show menu',
+//               );
+//             },
+//             menuChildren: [
+//               MenuItemButton(
+//                 onPressed: null,
+//                 child: Text('text'),
+//               ),
+//               MenuItemButton(
+//                 child: const Text('main'),
+//                 onPressed: () {
+//                   log.info('main pressed');
+//                 },
+//               )
+//             ],
+//           ),
+//         ],
+//         title: const Text('投資に関するアンケート'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(1.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           children: <Widget>[
+//             LinearProgressIndicator(
+//               // value: controller.value,
+//               value: i,
+//               semanticsLabel: 'Linear progress indicator',
+//             ),
+//             ElevatedButton(
+//                 onPressed: () {
+//                   i = i + 0.1;
+//                   // log.info('document : ${document.getBlocks()}');
+//                   document.getBlocks();
+//                 },
+//                 child: const Text('Forward')),
+//             ElevatedButton(
+//                 onPressed: () {
+//                   i = i - 0.1;
+//                 },
+//                 child: const Text('Backward')),
+//             Text('${i.toString()}'),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-  @override
-  State<SurveyMenu> createState() => _SurveyMenuState();
-}
-
-class _SurveyMenuState extends State<SurveyMenu> with TickerProviderStateMixin {
-  late AnimationController controller;
-
+class HomeScreen extends StatelessWidget {
+  /// Constructs a [HomeScreen]
+  HomeScreen({super.key});
   final Document document = Document();
-
-  var i = 0.0;
-
-  @override
-  void initState() {
-    controller = AnimationController(
-      /// [AnimationController]s can be created with `vsync: this` because of
-      /// [TickerProviderStateMixin].
-      vsync: this,
-      duration: const Duration(seconds: 120),
-    )..addListener(() {
-        setState(() {});
-      });
-    controller.repeat(reverse: true);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+  late List<Block> questions;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // title: const Text('Home Screen')
+
         actions: <Widget>[
           MenuAnchor(
             builder: (BuildContext context, MenuController controller,
@@ -114,8 +195,21 @@ class _SurveyMenuState extends State<SurveyMenu> with TickerProviderStateMixin {
             },
             menuChildren: [
               MenuItemButton(
-                onPressed: null,
-                child: Text('text'),
+                onPressed: () {
+                  document.getBlocks();
+                },
+                child: Text('getBlocks()'),
+              ),
+              MenuItemButton(
+                onPressed: () {
+                  final List<Block> questions = document.getBlocks();
+                  for (var q in questions) {
+                    log.info('--------------------------------------------');
+                    BlockWidget(block: q);
+                    log.info('============================================');
+                  }
+                },
+                child: Text('document'),
               ),
               MenuItemButton(
                 child: const Text('main'),
@@ -128,32 +222,95 @@ class _SurveyMenuState extends State<SurveyMenu> with TickerProviderStateMixin {
         ],
         title: const Text('投資に関するアンケート'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(1.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            LinearProgressIndicator(
-              // value: controller.value,
-              value: i,
-              semanticsLabel: 'Linear progress indicator',
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  i = i + 0.1;
-                  // log.info('document : ${document.getBlocks()}');
-                  document.getBlocks();
-                },
-                child: const Text('Forward')),
-            ElevatedButton(
-                onPressed: () {
-                  i = i - 0.1;
-                },
-                child: const Text('Backward')),
-            Text('${i.toString()}'),
-          ],
+      body: Center(
+        child: ElevatedButton(
+          // onPressed: () => context.go('/details'),
+          onPressed: () {},
+          child: const Text('Go to the Details screen'),
         ),
       ),
+    );
+  }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    required this.block,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: switch (block) {
+        Type70(:final comment, :final questionid, :final text) =>
+          Text('type 70'),
+        Type50(:final comment, :final questionid, :final text, :final nexts) =>
+          Text('type 50'),
+        Type31(
+          :final comment,
+          :final questionid,
+          :final text,
+          :final choices,
+          :final nexts
+        ) =>
+          Text('type 31'),
+        Type40(
+          :final comment,
+          :final questionid,
+          :final text,
+          :final values,
+          :final nexts
+        ) =>
+          Text('type 40'),
+        Type30(
+          :final comment,
+          :final questionid,
+          :final text,
+          :final choices,
+          :final nexts
+        ) =>
+          Text('type 30'),
+        Type20(
+          :final comment,
+          :final questionid,
+          :final text,
+          :final choices,
+          :final nexts
+        ) =>
+          Text('type 20'),
+        Type21(
+          :final comment,
+          :final questionid,
+          :final text,
+          :final choices,
+          :final nexts
+        ) =>
+          Text('type 21'),
+        Type60(:final comment, :final questionid, :final text, :final nexts) =>
+          Text('type 60'),
+        Type10(:final comment, :final questionid, :final text, :final nexts) =>
+          Text('type 10'),
+
+        // HeaderBlock(:final text) => Text(
+        //   text,
+        //   style: Theme.of(context).textTheme.displayMedium,
+        // ),
+        // ParagraphBlock(:final text) => Text(text),
+        // CheckboxBlock(:final text, :final isChecked) => Row(
+        //   children: [
+        //     Checkbox(value: isChecked, onChanged: (_) {}),
+        //     Text(text),
+        //   ],
+        // ),
+        // TODO: Handle this case.
+        Type31() => null,
+        // TODO: Handle this case.
+        Type40() => null,
+      },
     );
   }
 }
--- surveyapp/lib/questions.dart
+++ surveyapp/lib/questions.dart
@@ -31,14 +31,12 @@ class Document {
   }
 }
 
-const documentJson = '''
+const documentJson = """
 {
   "metadata": {
     "title": "投資に関するアンケート",
     "modified": "2024-01-01"
   },
-
-
   "questions" : [
     {
       "comment" : "10. 文書 + はい/いいえ",
@@ -47,7 +45,6 @@ const documentJson = '''
       "text" : "今後の情報提供に役立てるため、このアンケートは、皆様の投資に関する知識、認識を把握させていただきます。ご協力をお願いいたします。勝手ながら、ご回答者は20歳以上の方に限定させていただきます。２０歳以上の方ですか？",
       "nexts" : ["20" , "11"]
     },
-    
     {
       "comment" : "60. 文書 + 戻る",
       "questionid" : "11",
@@ -55,7 +52,6 @@ const documentJson = '''
       "text" : "勝手ながらご回答者を20歳以上の方に限定させていただきます。" ,
       "nexts" : ["10"]
     },
-
     {
       "comment" : "21. 文書 + 選択肢 (複数)",
       "questionid" : "20",
@@ -66,11 +62,10 @@ const documentJson = '''
        "藤黄色の銀行 f7c114",
         "千草色の銀行 92b5a9",
         "漆黒色の銀行 0d0015",
-        "その他の銀行",
+        "その他の銀行 0d0015"
       ],
       "nexts" : ["21"]
     },
-    
     {
       "comment" : "21. 文書 + 選択肢 (複数)",
       "questionid" : "21",
@@ -83,8 +78,7 @@ const documentJson = '''
       ],
       "nexts" : [ "30"]
     },
-
-     {
+    {
       "comment" : "20. 文書 + 選択肢 (一択)",
       "questionid" : "30",
       "type" : "20",
@@ -144,7 +138,7 @@ const documentJson = '''
   ]
 
 }
-''';
+""";
 
 sealed class Block {
   Block();
@@ -315,18 +309,18 @@ class Type10 extends Block {
   Type10(this.comment, this.questionid, this.text, this.nexts);
 }
 
-class HeaderBlock extends Block {
-  final String text;
-  HeaderBlock(this.text);
-}
-
-class ParagraphBlock extends Block {
-  final String text;
-  ParagraphBlock(this.text);
-}
-
-class CheckboxBlock extends Block {
-  final String text;
-  final bool isChecked;
-  CheckboxBlock(this.text, this.isChecked);
-}
+// class HeaderBlock extends Block {
+//   final String text;
+//   HeaderBlock(this.text);
+// }
+//
+// class ParagraphBlock extends Block {
+//   final String text;
+//   ParagraphBlock(this.text);
+// }
+//
+// class CheckboxBlock extends Block {
+//   final String text;
+//   final bool isChecked;
+//   CheckboxBlock(this.text, this.isChecked);
+// }
